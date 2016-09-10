ffa9322170ffbb7e5cfb480f0a1265ef8918f5a7
Ulric Qin <ulric.qin@gmail.com>
2016-09-10 16:48:53 +0800

    more safe check

--- httplib/post.go
+++ httplib/post.go
@@ -3,6 +3,7 @@ package httplib
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 )
@@ -18,6 +19,14 @@ func PostJSON(url string, v interface{}) (response []byte, err error) {
 	resp, err := http.Post(url, "application/json", bf)
 	if err != nil {
 		return response, err
+	}
+
+	if resp.StatusCode != 200 {
+		return []byte{}, errors.New("status code <> 200")
+	}
+
+	if resp.Body == nil {
+		return []byte{}, nil
 	}
 
 	defer resp.Body.Close()
